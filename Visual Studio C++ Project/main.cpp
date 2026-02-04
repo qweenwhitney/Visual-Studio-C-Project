e0ade7b88d912596e0fd8621c0821e6e9a3612fd
qween <qween@WHITS_ASUS>
2026-02-04 13:17:54 -0500

    Add project files.

---
+++ Visual Studio C++ Project/main.cpp
@@ -0,0 +1,10 @@
+// This is a simple C++ program that prints
+// "Hello, World!" to the console. 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
